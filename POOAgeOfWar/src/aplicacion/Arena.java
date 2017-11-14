50ed681e9eeb87364b911be8049a458878bf1f3f
nicolas torres <30741521+novenix@users.noreply.github.com>
2017-11-13 19:43:00 -0500

    Add files via upload

--- POOAgeOfWar/src/aplicacion/Arena.java
+++ POOAgeOfWar/src/aplicacion/Arena.java
@@ -1,5 +1,20 @@
 package aplicacion;
 
 public class Arena {
-
+	private Unidad[] arena;
+	public Arena(){
+		arena=new Unidad[20];
+		arena[0]=new Base(1,0);
+		arena[-1]=new Base(2,0);
+	}
+	public void mover(){}
+	
+	public void ponerUnidad(Base base,String tipo){
+		
+		Unidad uni=Base.crearUnidad(tipo);
+		int spawn=Base.getSpawn();
+		arena[spawn]=uni;
+		
+	}
+	
 }
