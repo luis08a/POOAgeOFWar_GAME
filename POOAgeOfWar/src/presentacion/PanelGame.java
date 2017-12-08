475c29407aea3f315bd70ab222cc4cc46a5267fd
luis08a <31138710+luis08a@users.noreply.github.com>
2017-12-08 17:39:10 -0500

    Update PanelGame.java

--- POOAgeOfWar/src/presentacion/PanelGame.java
+++ POOAgeOfWar/src/presentacion/PanelGame.java
@@ -15,11 +15,8 @@ public class PanelGame extends JPanel implements Runnable{
 	private Arena r;
 	private final int HEIGHT=Juego.ALTO*4/5;
 	private final int WIDTH=Juego.ANCHO;
-	private Sprite[] sprites = new Sprite[20]; 
-	private final Sprite[] bases = new Sprite[2];
 	private Image fondo = new ImageIcon(getClass().getResource("/recursos visuales/1.png")).getImage();;
 	private Thread thread;
-	private Thread pin;
 	private Unidad[][] u ;
 	private volatile boolean enEjecucion;
 	private boolean JCJ;
@@ -40,19 +37,15 @@ public class PanelGame extends JPanel implements Runnable{
 	}
 	
 	/*
-	 * Crea un Sprite (Dibujo) que será recreado en pantalla.
+	 * Crea un Sprite (Dibujo) que serÃ¡ recreado en pantalla.
 	 */
 	public void createSprite(String name,int num) {
 		Base b= r.getBase(num);
-		
-		
 		try {
 			r.ponerUnidad(b, "@");
 		} catch (PAOWException e) {
-			
 			//e.printStackTrace();
 		}
-		
 		repaint();
 	}
 	
@@ -81,9 +74,16 @@ public class PanelGame extends JPanel implements Runnable{
         	}
         	
         	if (u[i][1]!=null ) {
-        		Sprite n = new SpriteTest(u[i][1].getPosx(),u[i][1].getPosy()); 
+        		Sprite n = new SpriteTest(u[i][1].getPosx(),u[i][1].getPosy());
+        		Image im = n.getImage();
         		
-	            g2d.drawImage(n.getImage(), n.getX(),n.getY(),80,80, this);
+        		AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+        		tx.translate(-im.getWidth(null), 0);
+        		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        		
+        		BufferedImage ima = Sprite.toBufferedImage(im);
+        		Image image = op.filter(ima, null);
+	            g2d.drawImage(image, n.getX(),n.getY(),80,80, this);
         	}
         	
         } 
@@ -92,8 +92,16 @@ public class PanelGame extends JPanel implements Runnable{
         
 	}
 	
+	public BufferedImage flipHorizontal(BufferedImage src){
+	     AffineTransform tx=AffineTransform.getScaleInstance(1.0,-1.0);  //scaling
+	     tx.translate(0,-src.getHeight());  //translating
+	     AffineTransformOp tr=new AffineTransformOp(tx,null);  //transforming
+	     
+	     return tr.filter(src, null);  //filtering
+	    }
+	
 	/*
-	 * Crea una única instancia de PanelGame.
+	 * Crea una Ãºnica instancia de PanelGame.
 	 */
 	public static PanelGame getPanelGame(boolean jcj) {
 		if (pg==null) {
@@ -113,39 +121,22 @@ public class PanelGame extends JPanel implements Runnable{
 	}
 	
 	/*
-	 * Inicia el hilo de actualización de la pantalla
+	 * Inicia el hilo de actualizaciÃ³n de la pantalla
 	 */
 	private void iniciar(){
 		enEjecucion =true;
 		
 		thread=new Thread(this,"actualiz");
-		
 		thread.start();
-		
-		
 	}
+	
 	/*
-	 * (non-Javadoc)
-	 * @see java.lang.Runnable#run()
-	 */
-	public void  run(){		
-		while(enEjecucion) {			
-			try {				
-				Thread.sleep(1000);
-				
-				actualizar();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	/*
-	 * Detiene el hilo de actualización
+	 * Detiene el hilo de actualizaciÃ³n
 	 */
 	public void parar() {
-		enEjecucion = false;
 		try {
 			thread.join();
+			enEjecucion = false;
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -154,12 +145,24 @@ public class PanelGame extends JPanel implements Runnable{
 	/*
 	 * Actualiza la pantalla y la arena.
 	 */
-	private void actualizar(){		
+	private synchronized void actualizar(){
 		r.actualizar();
 		repaint();
 		//r.enemyIAIngenuo();
 	}
 	
-	
-	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void  run(){
+		while(enEjecucion) {
+			try {
+				Thread.sleep(1000);
+				actualizar();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
